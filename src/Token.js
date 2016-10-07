2d0beb3d75e8779846936310d6c40dd252b6b49f
lukelin <773000785@qq.com>
2016-10-07 23:04:14 +0800

    commit

--- src/Token.js
+++ src/Token.js
@@ -1,76 +1,161 @@
+const KEY_WORDS = 'if then else lambda func true false';
 
-export class Token {
-    constructor(line){
-        this.lineNumber = line;
-    }
-
-    isIdentifier(){
-        return false;
-    }
-
-    isNumber(){
-        return false;
-    }
-
-    isString(){
-        return false;
-    }
-
-    getText(){
-        return '';
-    }
-}
-Token.EOF = new Token(-1);
-Token.EOL = '\\n';
-
-export class NumToken extends Token {
-    constructor(line, v){
-        super(line);
-
-        this.value = v;
-    }
-
-    isNumber(){
-        return true;
-    }
-
-    getText(){
-        return this.value + '';
-    }
-
-    getNumber(){
-        return this.value;
-    }
+function isKeyword(value){
+    return KEY_WORDS.indexOf(` ${ value } `) >= 0;
 }
 
-export class IdToken extends Token {
-    constructor(line, id){
-        super(line);
-
-        this.text = id;
-    }
-
-    isIdentifier(){
-        return true;
-    }
-
-    getText(){
-        return this.text;
-    }
+function isDigit(value){
+    return /\d/.test(value);
 }
 
-export class StrToken extends Token {
-    constructor(line, str){
-        super(line);
+function isIdStart(value){
+    return /[a-z_]/i.test(value);
+}
 
-        this.literal = str;
+function isId(value){
+    return isIdStart(value) || '?!-<>=0123456789'.indexOf(ch) >= 0;
+}
+
+function isOpChar(value){
+    return '+-*/%=&|<>!'.indexOf(value) >= 0;
+}
+
+function isPunc(value){
+    return ',;(){}[]'.indexOf(value) >= 0;
+}
+
+function isWhitespace(value){
+    return '\t\n'.indexOf(value) >= 0;
+}
+
+function nonWrap(value){
+    return value !== '\n';
+}
+
+
+export default class Token {
+    constructor(input = ''){
+        this.input = input;
+        this.current = null;
     }
 
-    isString(){
-        return true;
+    next(){
+        let token = this.current;
+        this.current = null;
+
+        return token || this._readNext();
     }
 
-    getText(){
-        return this.literal;
+    _readNext(){
+        this._readWhile(isWhitespace);
+
+        if(this.input.eof()) return null;
+
+        let ch = this.input.peek();
+        if(ch === '#'){
+            this._skipComment();
+            return this._readNext();
+        }
+
+        if(ch === '"') return this._readString();
+        if(isDigit(ch)) return this._readNumber();
+        if(isIdStart(ch)) return this._readIdent();
+        if(isPunc(ch)) {
+            return {
+                type: 'punc',
+                value: this.input.next()
+            };
+        }
+        if(isOpChar(ch)){
+            return {
+                type: 'op',
+                value: this._readWhile(isOpChar)
+            };
+        }
+
+        this.input.croak(`Can't handle character: ${ ch }`);
+    }
+
+    _readWhile(predicate){
+        let str = '';
+        while(!this.input.eof() && predicate(this.input.peek())){
+            str += this.input.next();
+        }
+
+        return str;
+    }
+
+    _skipComment(){
+        this._readWhile(nonWrap);
+    }
+
+    _readString(){
+        return {
+            type: 'str',
+            value: this._readEscaped('"')
+        };
+    }
+
+    _readNumber(){
+        let hasDot = false;
+        let number = this._readWhile(function(ch){
+            if(ch === '.') {
+                if(hasDot) return false;
+                hasDot = true;
+                return true;
+            }
+
+            return isDigit(ch);
+        });
+
+        return {
+            type: 'number',
+            value: parseFloat(number)
+        };
+    }
+
+    _readIdent(){
+        let id = this._readWhile(isId);
+
+        return {
+            type: isKeyword(id) ? 'keyword' : 'variable',
+            value: id
+        };
+    }
+
+    _readEscaped(end){
+        let escaped = false;
+        let str = '';
+
+        this.input.next();
+
+        while(!this.input.eof()){
+            let ch = this.input.next();
+
+            if(escaped) {
+                str += ch;
+                escaped = false;
+            } else if(ch === '\\'){
+                escaped = true;
+            } else if(ch === end){
+                break;
+            } else {
+                str += ch;
+            }
+        }
+
+        return str;
+    }
+
+    peek(){
+        return this.current || (this.current = this._readNext());
+    }
+
+    eof(){
+        return this.peek() == null;
+    }
+
+    croak(){
+        return this.input.croak();
     }
 }
