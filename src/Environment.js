618cd44819b8d7a327121b5f4fa9ece69ad74240
LukeLin <773000785@qq.com>
2017-05-08 21:59:46 +0800

    update

---
+++ src/Environment.js
@@ -0,0 +1,42 @@
+class Environment {
+    constructor(parent){
+        this.vars = Object.create(parent ? parent.vars : null);
+        this.parent = parent;
+    }
+
+    extend(){
+        return new this.constructor(this);
+    }
+
+    lookup(name){
+        let scope = this;
+        while(scope){
+            if(Object.prototype.hasOwnProperty.call(scope.vars, name))
+                return scope;
+
+                scope = scope.parent;
+        }
+    }
+
+    get(name){
+        if(name in this.vars)
+            return this.vars[name];
+
+        throw Error(`Undefined variable ${ name }`);
+    }
+
+    set(name, value){
+        let scope = this.lookup(name);
+        // let's not allow defining globals from a nested environment
+        if(!scope && this.parent)
+            throw Error(`Undefined variable ${ name }`);
+
+        return (scope || this).vars[name] = value;
+    }
+
+    def(name, value){
+        return this.vars[name] = value;
+    }
+}
+
+module.exports = Environment;
